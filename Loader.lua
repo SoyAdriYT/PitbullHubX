2eaffed49e99807a1875fad22bce13ac3856eff5
Bélico2 <127357342+SoyAdriYT@users.noreply.github.com>
2024-07-20 03:36:53 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,32 @@
+local scriptURL = "https://raw.githubusercontent.com/SoyAdriYT/PitbullHubX/main/Games/Blade%20Ball.lua"
 
+local function executeScriptFromURL(url)
+    local success, response = pcall(function()
+        return game:HttpGet(url, true)
+    end)
+
+    if success then
+        local loadSuccess, loadError = pcall(function()
+            loadstring(response)()
+        end)
+
+        if loadSuccess then
+            print("Script executed successfully from URL: " .. url)
+        else
+            warn("Error executing the script: " .. loadError)
+        end
+    else
+        warn("Error fetching the script from URL: " .. url)
+    end
+end
+
+local function main()
+
+    print("Starting execution of the PitbullHubX script for Blade Ball...")
+
+    executeScriptFromURL(scriptURL)
+  
+    print("Finished execution of the script.")
+end
+
+main()
