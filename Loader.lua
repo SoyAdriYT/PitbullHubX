fd03656eabc53e17da70990683e23a2243301798
Bélico2 <127357342+SoyAdriYT@users.noreply.github.com>
2024-07-20 15:11:28 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -8,19 +8,24 @@
 ╰╯╱╱╰━━╯╱╰╯╱╰━━━┻━━━┻━━━┻━━━╯╰╯╱╰┻━━━┻━━━╯╰━╯╰━╯
 
 ]]--
+local gameId = game.PlaceId
 
-local scriptURL = "https://raw.githubusercontent.com/SoyAdriYT/PitbullHubX/main/Games/Identifier.lua"
+local scriptURLs = {
+    [4623386862] = "https://raw.githubusercontent.com/SoyAdriYT/PitbullHubX/main/Games/Piggy.lua",
+    [5661005779] = "https://raw.githubusercontent.com/SoyAdriYT/PitbullHubX/main/Games/Piggy.lua",
+    default = "https://raw.githubusercontent.com/SoyAdriYT/PitbullHubX/main/Games/Blade%20Ball.lua"
+}
 
 local function executeScriptFromURL(url)
     local success, response = pcall(function()
         return game:HttpGet(url, true)
     end)
-
+    
     if success then
         local loadSuccess, loadError = pcall(function()
             loadstring(response)()
         end)
-
+        
         if loadSuccess then
             print("Script executed successfully from URL: " .. url)
         else
@@ -32,12 +37,13 @@ local function executeScriptFromURL(url)
 end
 
 local function main()
-
-    print("Starting execution of the PitbullHubX script for Blade Ball...")
-
+    local scriptURL = scriptURLs[gameId] or scriptURLs.default
+    
+    print("Starting execution of PitbullHubX script for game with ID: " .. gameId)
+    
     executeScriptFromURL(scriptURL)
-  
-    print("Finished execution of the script.")
+    
+    print("Finished execution of script for game with ID: " .. gameId)
 end
 
 main()
