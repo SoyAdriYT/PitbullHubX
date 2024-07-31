c114312d5c4d939c582f619e225394ba9551e4e6
Bélico2 <127357342+SoyAdriYT@users.noreply.github.com>
2024-07-31 13:18:49 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,13 +1,3 @@
---[[
-
-╭━━━┳━━┳━━━━┳━━╮╭╮╱╭┳╮╱╱╭╮╱╱╱╭╮╱╭┳╮╱╭┳━━╮╱╭━╮╭━╮╭╮╱╱╭━━━┳━━━┳━━━┳━━━┳━━━╮
-┃╭━╮┣┫┣┫╭╮╭╮┃╭╮┃┃┃╱┃┃┃╱╱┃┃╱╱╱┃┃╱┃┃┃╱┃┃╭╮┃╱╰╮╰╯╭╯┃┃╱╱┃╭━╮┃╭━╮┣╮╭╮┃╭━━┫╭━╮┃
-┃╰━╯┃┃┃╰╯┃┃╰┫╰╯╰┫┃╱┃┃┃╱╱┃┃╱╱╱┃╰━╯┃┃╱┃┃╰╯╰╮╱╰╮╭╯╱┃┃╱╱┃┃╱┃┃┃╱┃┃┃┃┃┃╰━━┫╰━╯┃
-┃╭━━╯┃┃╱╱┃┃╱┃╭━╮┃┃╱┃┃┃╱╭┫┃╱╭╮┃╭━╮┃┃╱┃┃╭━╮┃╱╭╯╰╮╱┃┃╱╭┫┃╱┃┃╰━╯┃┃┃┃┃╭━━┫╭╮╭╯
-┃┃╱╱╭┫┣╮╱┃┃╱┃╰━╯┃╰━╯┃╰━╯┃╰━╯┃┃┃╱┃┃╰━╯┃╰━╯┃╭╯╭╮╰╮┃╰━╯┃╰━╯┃╭━╮┣╯╰╯┃╰━━┫┃┃╰╮
-╰╯╱╱╰━━╯╱╰╯╱╰━━━┻━━━┻━━━┻━━━╯╰╯╱╰┻━━━┻━━━╯╰━╯╰━╯╰━━━┻━━━┻╯╱╰┻━━━┻━━━┻╯╰━╯
-
-]]--
 local Players = game:GetService("Players")
 local kickk = Players.LocalPlayer
 local motivo = "Pitbull Hub X is temporarily disabled due to the recent anti-cheat update from Blade Ball. Please join our Discord server https://discord.gg/A4WzWGSgQj to stay informed about the latest news and updates regarding Pitbull Hub X. Rest assured, Pitbull Hub X will be back up and running very soon."
