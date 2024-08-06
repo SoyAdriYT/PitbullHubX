eba59eed4be9ed39a347e811060ddb240534498e
Bélico2 <127357342+SoyAdriYT@users.noreply.github.com>
2024-08-06 15:01:49 -0600

    Create Support.lua

---
+++ Scripts/Support.lua
@@ -0,0 +1,43 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/SoyAdriYT/PitbullHubX/main/Library/V4.lua"))()
+
+local function getExecutorName()
+    local executor = "Unknown"
+    
+    if getexecutorname then
+        executor = getexecutorname()
+    end
+    
+    return executor
+end
+
+local supportedExploits = {
+    "Cubix",
+    "Solara",
+    "Delta",
+    "Arceus X",
+    "Codex"
+}
+
+local executorName = getExecutorName()
+
+local isSupported = false
+for _, exploit in ipairs(supportedExploits) do
+    if executorName == exploit then
+        isSupported = true
+        break
+    end
+end
+
+if isSupported then
+    local Notify = Library:MakeNotify({
+        Title = "Support",
+        Text = "Your executor, " .. executorName .. ", is now listed among the supported exploits. You can enjoy Pitbull Hub X without any errors or issues.",
+        Time = 5
+    })
+else
+    local Notify = Library:MakeNotify({
+        Title = "Unsupported",
+        Text = "Your executor, " .. executorName .. ", was not found in the list of supported exploits, which may cause errors in Pitbull Hub.",
+        Time = 5
+    })
+end
