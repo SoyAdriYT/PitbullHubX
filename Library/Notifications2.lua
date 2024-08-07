1b58e26b02e18d3318f6a93425161ab213fd0876
Bélico2 <127357342+SoyAdriYT@users.noreply.github.com>
2024-08-07 17:58:01 -0600

    Update Notifications2.lua

--- Library/Notifications2.lua
+++ Library/Notifications2.lua
@@ -8,3 +8,4 @@
 ╰╯╱╱╰━━╯╱╰╯╱╰━━━┻━━━┻━━━┻━━━╯╰╯╱╰┻━━━┻━━━╯╰━╯╰━╯╰╯╱╰━┻━━━╯╱╰╯╱╰━━┻╯╱╱╰━━┻━━━┻╯╱╰╯╱╰╯╱╰━━┻━━━┻╯╱╰━┻━━━╯╰━━━╯
 
 ]]--
+local a=game:GetService("CoreGui"):FindFirstChild("STX_Nofitication")if not a then local b=Instance.new("ScreenGui")local c=Instance.new("UIListLayout")b.Name="STX_Nofitication"b.Parent=game.CoreGui;b.ZIndexBehavior=Enum.ZIndexBehavior.Sibling;b.ResetOnSpawn=false;c.Name="STX_NofiticationUIListLayout"c.Parent=b;c.HorizontalAlignment=Enum.HorizontalAlignment.Right;c.SortOrder=Enum.SortOrder.LayoutOrder;c.VerticalAlignment=Enum.VerticalAlignment.Bottom else end
